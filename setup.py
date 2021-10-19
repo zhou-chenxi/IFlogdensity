02a220eba594c4f33f5cb83cc3886d719d32c303
zhoucx1119 <zhoucx1989@gmail.com>
2021-10-19 16:21:55 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='IFdensity',
+	version='0.0.1',
+	packages=['IFdensity'],
+	url='',
+	license='MIT',
+	author='Chenxi Zhou',
+	author_email='zhou.779@osu.edu',
+	description='Computes and visualizes the influence function of score matching log density functions'
+)
