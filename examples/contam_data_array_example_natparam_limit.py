141d4b27e722c4f9e2c317e768ec6a902dec2b4d
zhoucx1119 <zhoucx1989@gmail.com>
2021-10-28 22:49:36 -0400

    Update contam_data_array_example_natparam_limit.py

--- examples/contam_data_array_example_natparam_limit.py
+++ examples/contam_data_array_example_natparam_limit.py
@@ -36,7 +36,8 @@ if __name__ == '__main__':
     base_density = BasedenGamma(np.load('data/geyser.npy').astype(np.float64)[:, 0])
     plot_kwargs = plot_IF_1d_params(x_limit=(21., 410.))
     
-    new_data = np.linspace(plot_kwargs['x_limit'][0], plot_kwargs['x_limit'][1], plot_kwargs['plot_pts_cnt'])
+    new_data = np.linspace(
+        plot_kwargs['x_limit'][0], plot_kwargs['x_limit'][1], plot_kwargs['plot_pts_cnt']).reshape(-1, 1)
     
     for contam_data in contam_data_array:
 
@@ -57,10 +58,10 @@ if __name__ == '__main__':
             bw=bw,
             kernel_type='gaussian_poly2')
         
-        ifun_natparam_file_name = f'/contam_data={contam_data_array[i]}-IF-natparam-newdata.npy'
+        ifun_natparam_file_name = f'/contam_data={contam_data[0]}-IF-natparam-newdata.npy'
         ifun_natparam = ifun.eval_IF_natparam(new_data=new_data)
 
-        ifun_natparamlimit_file_name = f'/contam_data={contam_data_array[i]}-IF-natparam-limit-newdata.npy'
+        ifun_natparamlimit_file_name = f'/contam_data={contam_data[0]}-IF-natparam-limit-newdata.npy'
         ifun_natparam_limit = ifun.eval_IF_natparam_limit(new_data=new_data)
         
         save_dir = f'data/bw={bw}-kernel={kernel_type}-loglambda={log_pen_param}-contamweight={contam_weight}'
